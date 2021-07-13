a40e8ad25d641f26641e152fc781d98e58f02cd9
DavidVallecios <00078217@uca.edu.sv>
2021-07-12 23:50:33 -0600

    SplashScreen finished

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import SplashScreen from './src/screens/SplashScreen';
+
+
+const App = () => {
+  return (
+      <SplashScreen />
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
+// #6C5CE8
