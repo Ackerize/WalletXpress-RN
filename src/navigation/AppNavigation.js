2a5f07375b7ed380c05bafba4b3f6a1d92b0a3fa
DavidVallecios <00078217@uca.edu.sv>
2021-07-16 16:32:40 -0600

    Terminando estilizacion del navbar

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,56 +1,77 @@
-import React from 'react';
-import { AnimatedTabBarNavigator } from 'react-native-animated-nav-tab-bar';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import Home from '../screens/HomeScreen';
-import Statistic from '../screens/StatisticScreen';
-import History from '../screens/HistoryScreen';
-import Profile from '../screens/ProfileScreen';
+import React from 'react'
+import { AnimatedTabBarNavigator } from 'react-native-animated-nav-tab-bar'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Home from '../screens/HomeScreen'
+import Statistic from '../screens/StatisticScreen'
+import History from '../screens/HistoryScreen'
+import Profile from '../screens/ProfileScreen'
 
-const Tabs = AnimatedTabBarNavigator();
+const Tabs = AnimatedTabBarNavigator()
 
 const TabBarIcon = ({ name, size, tintColor }) => (
-  <Icon name={name} size={size ? size : 24} color={tintColor} />
-);
+	<Icon name={name} size={size ? size : 24} color={tintColor} />
+)
 
 export default function AppNavigation() {
-  return (
-    <Tabs.Navigator>
-      <Tabs.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <TabBarIcon focused={focused} tintColor={color} name="home" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Statistic"
-        component={Statistic}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <TabBarIcon focused={focused} tintColor={color} name="equalizer" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="History"
-        component={History}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <TabBarIcon focused={focused} tintColor={color} name="history" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <TabBarIcon focused={focused} tintColor={color} name="person" />
-          ),
-        }}
-      />
-    </Tabs.Navigator>
-  );
-};
+	return (
+		<Tabs.Navigator
+			tabBarOptions={{
+				activeBackgroundColor: '#DBF2EC',
+				activeTintColor: '#478B91',
+				inactiveTintColor: '#9E9EA7',
+			}}>
+			<Tabs.Screen
+				name="Home"
+				component={Home}
+				options={{
+					tabBarIcon: ({ focused, color }) => (
+						<TabBarIcon
+							focused={focused}
+							tintColor={color}
+							name="home"
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="Statistic"
+				component={Statistic}
+				options={{
+					tabBarIcon: ({ focused, color }) => (
+						<TabBarIcon
+							focused={focused}
+							tintColor={color}
+							name="equalizer"
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="History"
+				component={History}
+				options={{
+					tabBarIcon: ({ focused, color }) => (
+						<TabBarIcon
+							focused={focused}
+							tintColor={color}
+							name="history"
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="Profile"
+				component={Profile}
+				options={{
+					tabBarIcon: ({ focused, color }) => (
+						<TabBarIcon
+							focused={focused}
+							tintColor={color}
+							name="person"
+						/>
+					),
+				}}
+			/>
+		</Tabs.Navigator>
+	)
+}
